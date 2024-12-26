6746ef50a7a75f73863b281271714d2358602a35
mszahan <mszahan1@gmail.com>
2024-12-26 11:58:08 +0600

    explored tkinter widgets

---
+++ gui_with_tkinter/ch_02_tkinter_concepts/02_tkinter_widgets.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+button = ttk.Button(root, text='Click here')
+button.pack()
+
+## you can access the button properties like this
+print(button['text'])
+## change the properties
+button['text'] = 'Press here'
+## here is another way to access the button properties
+button.config(text='Press me')
+
+## you can get all the properties of the button
+print(button.config())  # returns a dictionary
+
+## if you need the file to work you need the line
+root.mainloop()
