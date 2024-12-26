b6e590c5309b05774d703f1a41fd5fc576962b56
mszahan <mszahan1@gmail.com>
2024-12-26 13:53:44 +0600

    made interactive hello world

--- gui_with_tkinter/ch_02_tkinter_concepts/02_tkinter_widgets.py
+++ gui_with_tkinter/ch_02_tkinter_concepts/02_tkinter_widgets.py
@@ -16,5 +16,11 @@ button.config(text='Press me')
 ## you can get all the properties of the button
 print(button.config())  # returns a dictionary
 
+## you can ge the name of the widget
+print(str(button))
+
+## add another label to the root
+label = ttk.Label(root, text='Hello, World!').pack()
+
 ## if you need the file to work you need the line
 root.mainloop()
